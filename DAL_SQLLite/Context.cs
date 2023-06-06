13fdc77b48f9aacdf2ad95c2e5a6f7cd54d9d8e1
DartBenSecond <ryabchikk@rambler.ru>
2023-06-06 21:18:04 +0300

    добавил базу-без проверки

---
+++ DAL_SQLLite/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_SQLLite
+{
+    public class Costs : DbContext
+    {
+        public DbSet<Spend> Spending { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public Costs()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=SQBase.db");
+        }
+    }
+}
