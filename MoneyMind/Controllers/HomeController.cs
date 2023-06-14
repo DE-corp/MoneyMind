52ac91d9a71a1e0ca87fb064d9a95e29a0838117
DE-corp <crazyHOPHOP@rambler.ru>
2023-06-15 01:20:32 +0300

    Исправил инициализацию базы данных при запуске проекта + тестовое добавление с формы расходов в базу

--- MoneyMind/Controllers/HomeController.cs
+++ MoneyMind/Controllers/HomeController.cs
@@ -1,22 +1,25 @@
-﻿using DAL_SQLLite;
+﻿using DAL_SQLLite.Models;
+using DAL_SQLLite.Repository;
 using Microsoft.AspNetCore.Mvc;
 using MoneyMind.Models;
+using MoneyMind.ViewModels;
 using System.Diagnostics;
 
 namespace MoneyMind.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository<Spend> _spendRepo;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepository<Spend> spendRepo)
         {
             _logger = logger;
+            _spendRepo = spendRepo;
         }
 
-        public IActionResult Index(CostsDbContext costsDb)
+        public IActionResult Index()
         {
-            costsDb.Spends.AsQueryable().Count();
             return View();
         }
 
@@ -29,6 +32,27 @@ namespace MoneyMind.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddSpend(AddSpendViewModel model)
+        {
+            var spend = new Spend
+            {
+                Cost = Convert.ToDecimal(model.SpendCost),
+                Category = new Category { Name = model.SpendCategory }
+            };
+
+            _spendRepo.Create(spend);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Spend>> GetAllSpends()
+        {
+            var spends = _spendRepo.GetAll();
+            return spends.ToList();
         }
     }
 }
---
+++ MoneyMind/ViewModels/AddSpendViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyMind.ViewModels
+{
+    public class AddSpendViewModel
+    {
+        public required string SpendCost { get; set; }
+        public required string SpendCategory { get; set; }
+
+    }
+}
